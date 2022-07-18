3cd1134742550a956d2560e5b6e6ff5268ebbb65
AmantinaR <amantinarossi@gmail.com>
2022-07-18 10:51:02 -0700

    created navbar component and updated app and hero

--- bucket-hero-ui/src/App.jsx
+++ bucket-hero-ui/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Hero from './components/Hero';
+import NavBar from './components/NavBar';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Hero/>
+    <>
+      <NavBar/>
+      <Hero/>
+      
+    </>
+    
   )
 }
 
