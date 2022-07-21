e39dd4d2b0b5f85c42c7733b4afac6928e72d245
AmantinaR <amantinarossi@gmail.com>
2022-07-21 11:05:48 -0700

    added new favicon and did more styling

--- bucket-hero-ui/src/components/Hero.jsx
+++ bucket-hero-ui/src/components/Hero.jsx
@@ -2,6 +2,7 @@
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import heroExtra from '../assets/heroextra.png'
 
 const navigation = [
   { name: 'Product', href: '#' },
@@ -12,28 +13,18 @@ const navigation = [
 
 export default function Hero() {
   return (
-    <div className="relative bg-white overflow-hidden">
-      <div className="max-w-7xl mx-auto">
+    <div className="relative bg-white overflow-hidden ">
+      <div className="max-w-7xl mx-auto flex flex-row items-center">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          <svg
-            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
-            fill="currentColor"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="none"
-            aria-hidden="true"
-          >
-            <polygon points="50,0 100,0 50,100 0,100" />
-          </svg>
 
-          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+          <main className=" mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 ">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                 <span className="block xl:inline">Prioritize your college experience with a </span>{' '}
                 <span className="block text-orange-600 xl:inline">bucket list</span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                fugiat veniam occaecat fugiat aliqua.
+                Bucket Hero will help you organize all of the events, locations, clubs, experiences, and more that you may want to explore and try in your time at school. Go outside of your bubble, get social, and get started now!
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
@@ -54,8 +45,10 @@ export default function Hero() {
                 </div>
               </div>
             </div>
+            
           </main>
         </div>
+        <img src={heroExtra} alt="" className='h-auto w-1/2 md:w-1/3 md:h-full hidden sm:hidden md:block lg:block'/>
       </div>
       
     </div>
--- bucket-hero-ui/src/components/ListCol.jsx
+++ bucket-hero-ui/src/components/ListCol.jsx
@@ -9,7 +9,7 @@ import {useNavigate} from 'react-router-dom';
 
 export default function ListCol({}) {
     const navigate = useNavigate();
-    const {lists, selected, setSelected, setBlTitle} = useDashContext();
+    const {lists, selected, setSelected, setBlTitle, setModalOpen} = useDashContext();
 
     useEffect(() => {
         if(!selected){
@@ -23,7 +23,7 @@ export default function ListCol({}) {
             <div className='flex flex-row items-center w-full justify-between mb-4 mt-2'>
                 <h2 className='text-xl font-semibold'>Your Lists</h2>
                 <div className='justify-self-end flex items-center'>
-                    <button className='flex bg-orange-400 rounded p-1' onClick={() => setModalOpen(true)}>
+                    <button className='flex bg-orange-400 rounded p-1' onClick={() => navigate('/newlist')}>
                         <p className='text-white'>Add</p>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="white" strokeWidth="2">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
