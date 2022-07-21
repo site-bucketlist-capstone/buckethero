d14fb6b54bc51332e1f916367784226c36be9770
AmantinaR <amantinarossi@gmail.com>
2022-07-21 09:35:44 -0700

    styling the lists

--- bucket-hero-ui/src/components/ListItems.jsx
+++ bucket-hero-ui/src/components/ListItems.jsx
@@ -26,13 +26,13 @@ export default function ListItems({}) {
     }, [selected]);
 
     return (
-        <div className='border border-blue-500 w-2/3 p-2 h-full'>
-            <div className='flex flex-row w-full my-2 border items-center justify-between px-4'>
+        <div className='w-2/3 p-4 h-full'>
+            <div className='flex flex-row w-full my-2 items-center justify-between'>
                 <h2 className='text-xl font-semibold'>{blTitle}</h2>
-                <div className='justify-self-end flex items-center border'>
-                    <button className='flex border border-violet-500 rounded' onClick={() => setModalOpen(true)}>
-                        <p>Add</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                <div className='justify-self-end flex items-center'>
+                    <button className='flex bg-purple-400 rounded p-1' onClick={() => setModalOpen(true)}>
+                        <p className='text-white'>Add</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="white" strokeWidth="2">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
                         </svg>
                     </button>
