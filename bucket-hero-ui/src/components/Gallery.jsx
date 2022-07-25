f8098ac4f1f213a45f0e09b50ade8df7503988f9
AmantinaR <amantinarossi@gmail.com>
2022-07-25 15:21:08 -0700

    gallery styling

--- bucket-hero-ui/src/components/Gallery.jsx
+++ bucket-hero-ui/src/components/Gallery.jsx
@@ -5,14 +5,19 @@ export default function Gallery({}) {
     const {gallery} = useGallContext();
 
     return (
-        <div className=''>
-            <h3>Gallery</h3>
-            <p>Gain items to put on your own list and gain inspiration!</p>
-            {
-                gallery.map((item) => {
-                   return <GalleryListItems item={item} />
-                })
-            }
+        <div className='flex flex-col items-center'>
+            <div className="w-full px-12">
+                <h3 className="text-2xl font-semibold">Gallery</h3>
+                <p>Gain items to put on your own list and gain inspiration!</p>
+            </div>
+            <div className="w-3/4 mt-4">
+                {
+                    gallery.map((item) => {
+                    return <GalleryListItems item={item} />
+                    })
+                }
+            </div>
+            
         </div>
     );
 }
