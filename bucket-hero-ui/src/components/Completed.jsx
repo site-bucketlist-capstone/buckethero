7c32d5a17750174095c6ba473bf54d0d5d0e6777
AmantinaR <amantinarossi@gmail.com>
2022-07-25 14:12:57 -0700

    completed items component done

--- bucket-hero-ui/src/components/Completed.jsx
+++ bucket-hero-ui/src/components/Completed.jsx
@@ -1,4 +1,5 @@
 import { useDashContext } from "../contexts/dashboard";
+import ListItemComp from "./ListItemComp";
 
 export default function Completed({completed}) {
     const {lists} = useDashContext();
@@ -12,16 +13,22 @@ export default function Completed({completed}) {
     function withinYear() {
         //count the num of completed items that are within start and end of current year
     }
+    
     return (
-        <div className=''>
-            <h3>Completed Items</h3>
-            
+        <div className='mt-4 flex flex-col items-center px-48 mb-8'>
+            <div className="bg-slate-200 p-6 w-1/2 flex flex-col rounded items-center">
+                <p className="text-lg font-semibold">Number of completed items:</p>
+                <p className="text-3xl font-bold">{completed.length}</p>
+            </div>
             {
                 lists.map((list) => {
                    return (
-                   <div key={list.id}>
-                       {list.name}
-                       {completed.map((item) => {if (item.list_id === list.id) return <div key={item.id}>{item.name}</div>})}
+                   <div key={list.id} className='border p-2 rounded w-full mt-4'>
+                       <div className="flex flex-row">
+                            {list.emoji_unicode ? <p className='mr-2 text-xl' dangerouslySetInnerHTML={{__html : `&#x${list.emoji_unicode};`}}></p> : null} 
+                            <p className="text-lg font-semibold">{list.name}</p>
+                       </div>
+                       {completed.map((item) => {if (item.list_id === list.id) return <div key={item.id}><ListItemComp item={item}/></div>})}
                    </div>)
                 })
             }
