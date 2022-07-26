181825c0f064994f16dfaa11c52e2f6a23c7075b
abdullahsal702 <88620119+abdullahsal702@users.noreply.github.com>
2022-07-26 12:17:50 -0400

    profile image backend complete

--- buckethero-api/models/profile.js
+++ buckethero-api/models/profile.js
@@ -10,7 +10,7 @@ class Profile {
             WHERE email = $2
             RETURNING email, 
                      profile_image
-         `, [imageString, user.email]
+         `, [imageString.imageString, user.email]
         )
 
         return results.rows[0]
