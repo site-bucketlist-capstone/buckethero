a43e36084a918d348fb1aeaee98796a69895df2e
issaice <hawo1551issa@gmail.com>
2022-08-01 13:11:28 -0700

    Users are able to edit profile

--- buckethero-api/models/list.js
+++ buckethero-api/models/list.js
@@ -17,7 +17,6 @@ class List {
    }
 
    static async createNewList({ list, user }) {
-      console.log(list.name);
       if (!list.name) {
          throw new BadRequestError("Missing name of list in request.");
       }
@@ -61,6 +60,7 @@ class List {
       const resultInfo = await db.query(
          `SELECT name, emoji_unicode FROM lists WHERE id = $1 `, [listId]
       )
+      console.log(resultInfo.rows);
       const name = resultInfo.rows[0].name
       const emoji_unicode = resultInfo.rows[0].emoji_unicode
       const result = await db.query(
--- buckethero-api/models/profile.js
+++ buckethero-api/models/profile.js
@@ -1,4 +1,6 @@
 const db = require("../db")
+const bcrypt = require("bcrypt")
+const { BCRYPT_WORK_FACTOR } = require("../config")
 const { BadRequestError, NotFoundError} = require("../utils/errors")
 
 class Profile {
@@ -11,6 +13,36 @@ class Profile {
             RETURNING email, 
                      profile_image
          `, [imageString.imageString, user.email]
+        )
+
+        return results.rows[0]
+    }
+
+    static async updateProfile({ profileUpdate, user }) {
+        const resultInfo = await db.query(
+            `SELECT first_name, last_name, email, password FROM users WHERE email = $1 `, [user.email]
+        )    
+        const first_name = resultInfo.rows[0].first_name;
+        const last_name = resultInfo.rows[0].last_name;
+        const email = resultInfo.rows[0].email;
+        const password = resultInfo.rows[0].password;
+        const hashedPassword = await bcrypt.hash(profileUpdate.password, BCRYPT_WORK_FACTOR)
+        const results = await db.query(
+            `
+            UPDATE users
+            SET first_name = $2, last_name = $3, email = $4, password = $5    
+            WHERE email = $1
+            RETURNING first_name,
+                      last_name,
+                      email,
+                      password
+         `, [
+            user.email,
+            profileUpdate.first_name || first_name ,
+            profileUpdate.last_name || last_name,
+            profileUpdate.email || email,
+            hashedPassword || password
+        ]
         )
 
         return results.rows[0]
