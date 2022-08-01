71fcde71fb3ad7d20eec66dac04248cf57a09aca
issaice <hawo1551issa@gmail.com>
2022-08-01 10:19:32 -0700

    Updated models/gallery

--- buckethero-api/models/gallery.js
+++ buckethero-api/models/gallery.js
@@ -3,9 +3,6 @@ const { NotFoundError } = require("../utils/errors")
 
 class Gallery {
    static async getGallery({user}) {
-      //this.createNewGallery({user});
-      //const items = await this.fetchItemsNotOwnedByUser(user);
-      //console.log(items);
       const results = await db.query (
          `
          SELECT g.id,
@@ -15,49 +12,47 @@ class Gallery {
          FROM gallery_items AS g
          `
       )  
-      console.log(results.rows);
       return results.rows;
    }
 
-   static async fetchItemsNotOwnedByUser(user) {
-      const results = await db.query(
-          `
-              SELECT  list_items.id,
-                      list_items.name, 
-                      list_items.category, 
-                      list_items.location, 
-                      users.first_name,
-                      users.last_name
-              FROM list_items 
-               JOIN users 
-              WHERE users.email != $1
-          `, [user.email]
-      )
-      const items = results.rows;
-      if (!items) {
-          throw new NotFoundError()
-      }
-      return items;
-  }
+//    static async fetchItemsNotOwnedByUser(user) {
+//       const results = await db.query(
+//           `
+//               SELECT  list_items.id,
+//                       list_items.name, 
+//                       list_items.category, 
+//                       list_items.location, 
+//                       users.first_name,
+//                       users.last_name
+//               FROM list_items 
+//                JOIN users 
+//               WHERE users.email != $1
+//           `, [user.email]
+//       )
+//       const items = results.rows;
+//       if (!items) {
+//           throw new NotFoundError()
+//       }
+//       return items;
+//   }
 
-   static async createNewGallery({user}) {
-      const items = await this.fetchItemsNotOwnedByUser({user});
-      console.log(items);
-      items.map(async (item) => {
-         let result = await db.query(
-            `
-               IF NOT EXISTS (SELECT name FROM gallery_items WHERE name = $1)
-                INSERT INTO gallery_items (name, location, category)
-                VALUES($1, $2, $3)
-                RETURNING id,
-                            name, location, category
-            `, [item.name, item.location, item.category]
-         )
-         console.log(result.rows);
-      })
-      ///console.log(results.rows[0]);
+//    static async createNewGallery({user}) {
+//       const items = await this.fetchItemsNotOwnedByUser({user});
+//       console.log(items);
+//       items.map(async (item) => {
+//          let result = await db.query(
+//             `
+//                IF NOT EXISTS (SELECT name FROM gallery_items WHERE name = $1)
+//                 INSERT INTO gallery_items (name, location, category)
+//                 VALUES($1, $2, $3)
+//                 RETURNING id,
+//                             name, location, category
+//             `, [item.name, item.location, item.category]
+//          )
+         
+//       })
       
-   }
+//    }
 }
 
 module.exports = Gallery;
