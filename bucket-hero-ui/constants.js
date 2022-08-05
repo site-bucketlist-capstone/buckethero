eddcc31d940a6249664076953e4a56fbd2f6090b
AmantinaR <amantinarossi@gmail.com>
2022-08-05 11:46:04 -0700

    added backend heroku link to apiClient

--- bucket-hero-ui/constants.js
+++ bucket-hero-ui/constants.js
@@ -1,5 +1,5 @@
-const PRODUCTION_API_BASE_URL = "http://localhost:3001";
-const DEVELOPMENT_API_BASE_URL = "http://localhost:3001";
+const PRODUCTION_API_BASE_URL = "https://buckethero.herokuapp.com/";
+const DEVELOPMENT_API_BASE_URL = "https://buckethero.herokuapp.com/";
 const API_BASE_URL = process.env.NODE_ENV === "production" ? PRODUCTION_API_BASE_URL : DEVELOPMENT_API_BASE_URL;
 //Heroku backend deployed but not working. Link : https://francophone-zed-87469.herokuapp.com/
 
